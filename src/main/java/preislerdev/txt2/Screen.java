c3388e67af49a2954c336d0fc672e765ba089a55
Preisler <preisler.andras@gmail.com>
2023-04-17 14:21:55 +0200

    main

--- src/main/java/preislerdev/txt2/Screen.java
+++ src/main/java/preislerdev/txt2/Screen.java
@@ -1,16 +1,29 @@
 package preislerdev.txt2;
 
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-public class Screen {
-    private Stage stage;
-    private Scene scene;
+import java.io.IOException;
+import java.util.Objects;
 
-    public Screen(Stage stage) {
-        this.stage = stage;
+public class Screen {
+    private static Stage stage;
+    private static Scene scene;
+
+    public static void setStage(Stage stage) {
+        Screen.stage = stage;
     }
-    public void setScene(Scene scene) {
+    public static void startFromLoader(String filename, String stylename) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(Txt2App.class.getResource(filename));
+        scene = new Scene(fxmlLoader.load());
+        scene.getStylesheets().add(Screen.class.getResource(stylename).toExternalForm());
+        stage.setTitle("TXT2");
+        stage.getIcons().add(new javafx.scene.image.Image(Objects.requireNonNull(Screen.class.getResourceAsStream("icon.png"))));
+        Screen.setScene(scene);
+    }
+
+    public static void setScene(Scene scene) {
         stage.setScene(scene);
         stage.show();
     }
--- src/main/java/preislerdev/txt2/Txt2App.java
+++ src/main/java/preislerdev/txt2/Txt2App.java
@@ -10,14 +10,8 @@ import java.io.IOException;
 public class Txt2App extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-
-        FXMLLoader fxmlLoader = new FXMLLoader(Txt2App.class.getResource("txt2.fxml"));
-        Scene scene = new Scene(fxmlLoader.load());
-        scene.getStylesheets().add(getClass().getResource("style.css").toExternalForm());
-        stage.setTitle("TXT2");
-        stage.getIcons().add(new javafx.scene.image.Image(getClass().getResourceAsStream("icon.png")));
-        stage.setScene(scene);
-        stage.show();
+        Screen.setStage(stage);
+        Screen.startFromLoader("txt2.fxml", "style.css");
     }
 
     public static void main(String[] args) {
